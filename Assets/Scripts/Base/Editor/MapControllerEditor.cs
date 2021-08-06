ce036ca9aa451b53312f97b8ac26c8917b0fc327
SirKarpov <sirmkarpov@gmail.com>
2021-08-06 19:06:36 +0300

    - upd

--- Assets/Scripts/Base/Editor/MapControllerEditor.cs
+++ Assets/Scripts/Base/Editor/MapControllerEditor.cs
@@ -3,29 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-//[CustomEditor(typeof(MapController))]
-//public class MapControllerEditor : Editor
-//{
-//    public override void OnInspectorGUI()
-//    {
-//        base.OnInspectorGUI();
+[CustomEditor(typeof(MapController))]
+public class MapControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
 
-//        MapController myTarget = (MapController)target;
+        MapController myTarget = (MapController)target;
 
-//        if (GUILayout.Button("ApplyChanges"))
-//        {
-//            myTarget.ApplyMapChanges();
-//            EditorUtility.SetDirty(myTarget);
-//            for (int i = 0; i < myTarget.MapCells.Count; i++)
-//            {
-//                EditorUtility.SetDirty(myTarget.MapCells[i]);
-//            }
-//        }
+        if (GUILayout.Button("ApplyChanges"))
+        {
+            myTarget.ApplyMapChanges();
+            EditorUtility.SetDirty(myTarget);
+            for (int i = 0; i < myTarget.MapCells.Count; i++)
+            {
+                EditorUtility.SetDirty(myTarget.MapCells[i]);
+            }
+            for (int i = 0; i < myTarget.Buildings.Count; i++)
+            {
+                EditorUtility.SetDirty(myTarget.Buildings[i]);
+            }
+        }
 
-//        if (GUILayout.Button("Clear"))
-//        {
-//            myTarget.ClearPrefs();        
-//        }
-//    }
+        if (GUILayout.Button("Clear"))
+        {
+            myTarget.ClearPrefs();
+            EditorUtility.SetDirty(myTarget);
+            for (int i = 0; i < myTarget.MapCells.Count; i++)
+            {
+                EditorUtility.SetDirty(myTarget.MapCells[i]);
+            }
+            for (int i = 0; i < myTarget.Buildings.Count; i++)
+            {
+                EditorUtility.SetDirty(myTarget.Buildings[i]);
+            }
+        }
+    }
 
-//}
+}
