580040aa06a14bd3a023f995007a370479e2d901
SirKarpov <sirmkarpov@gmail.com>
2021-09-01 11:20:08 +0300

    - upd

--- Assets/Scripts/Raids/ResourceSpot.cs
+++ Assets/Scripts/Raids/ResourceSpot.cs
@@ -5,22 +5,25 @@ using UnityEngine;
 
 public class ResourceSpot : MonoBehaviour
 {
+    public const int COUNT_PER_USE = 1;
+
     public event System.Action<ResourceSpot> OnSpotUsed;
     [SerializeField] int maxUses = 3;
-    [SerializeField] int countPerUse = 3;
     [SerializeField] ResourceType resourceType;
     [SerializeField] float noisePerUse = 10f;
     [SerializeField] float resourcesVelocity = 1f;
     [SerializeField] Vector3 resourceSpawnOffset = new Vector3(0f, 1f, 0f);
-    [SerializeField] ResourceInteractionType interactionType;   
+    [SerializeField] ResourceInteractionType interactionType;
 
+    float scale = 0;
     public ResourceInteractionType InteractionType => interactionType;
 
     int uses = 0;
 
     private void Awake()
     {
-        Level.Instance.RegisterResourceSpot(this);       
+        scale = transform.localScale.magnitude;
+        Level.Instance.RegisterResourceSpot(this);
     }
 
     public void UseSpot(GameObject user)
@@ -35,18 +38,18 @@ public class ResourceSpot : MonoBehaviour
         Level.Instance.AddNoiseLevel(noisePerUse);
 
         transform.DOKill(true);
-        transform.DOPunchScale(Vector3.one * 0.1f, 0.1f, 3);
+        transform.DOPunchScale(Vector3.one * 0.1f * scale, 0.2f, 3);
 
         if (uses >= maxUses)
         {
-            OnSpotUsed?.Invoke(this);            
+            OnSpotUsed?.Invoke(this);
             transform.DOScale(0f, 0.25f).SetEase(Ease.InCirc).OnComplete(() => Destroy(gameObject));
         }
     }
 
     void SpawnResources(GameObject user)
     {
-        for (int i = 0; i < countPerUse; i++)
+        for (int i = 0; i < COUNT_PER_USE; i++)
         {
             Resource instance = Instantiate(resourceType.defaultPrefab, transform.position + resourceSpawnOffset, transform.rotation);
             instance.PickUp(user.transform);
