0cd0d8118dd904060636853c66e4d8f31d755a7a
SirKarpov <sirmkarpov@gmail.com>
2021-09-02 22:08:28 +0300

    - upd

---
+++ Assets/Scripts/Raids/Tutorial/TutorialUIHand.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TutorialUIHand : MonoBehaviour
+{
+    [SerializeField] float timeToShow = 2f;
+    [SerializeField] ConditionTrigger disableTrigger;
+    [SerializeField] Transform content;
+
+    Squad squad;
+    float notMovingTime;
+
+    Vector3 scale;
+
+
+    private void Awake()
+    {
+        scale = transform.localScale;
+        squad = FindObjectOfType<Squad>();
+        disableTrigger.OnTrigger += DisableTrigger_OnTrigger;
+        content.gameObject.SetActive(false);
+    }
+
+    private void DisableTrigger_OnTrigger()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!squad.IsMoving)
+        {
+            if (!content.gameObject.activeSelf)
+            {
+                notMovingTime += Time.deltaTime;
+                if (notMovingTime >= timeToShow)
+                {
+                    Show();
+                }
+            }
+        }
+        else
+        {
+            if (content.gameObject.activeSelf)
+            {               
+                notMovingTime = 0f;
+                Hide();
+            }
+        }
+       
+    }
+
+    void Show()
+    {
+        content.DOKill(true);
+        content.gameObject.SetActive(true);
+        content.localScale = Vector3.zero;
+        content.DOScale(scale, 0.3f).SetEase(Ease.OutCirc);
+    }
+
+    void Hide()
+    {
+        content.DOKill(true);
+        content.DOScale(Vector3.zero, 0.3f).SetEase(Ease.InCirc).OnComplete(() => content.gameObject.SetActive(false));
+    }
+
+}
