32912f68bbc9c58926e7ad0968f6c1e045639a43
SirKarpov <sirmkarpov@gmail.com>
2021-12-08 18:23:46 +0300

    - upd

--- Assets/Scripts/Raids/HordeController.cs
+++ Assets/Scripts/Raids/HordeController.cs
@@ -25,9 +25,27 @@ public class HordeController : MonoBehaviour
     [SerializeField] HordeMovementSettings avoidance = new HordeMovementSettings(true, 2f, 0.5f);
     [SerializeField] bool alignmentAlternative = false;
     [SerializeField] bool correctSpeed = false;
+    [SerializeField] Vector3 begin;
+    [SerializeField] Vector3 end;
+    [SerializeField] float step = 3f;  
+    
 
     List<ZombieMovement> zombieMovements = new List<ZombieMovement>();
+  
+    List<int>[] buckets;
 
+    int sizeX, sizeY; 
+
+    private void Awake()
+    {
+        sizeX = Mathf.FloorToInt((end.x - begin.x) / step) + 1;
+        sizeY = Mathf.FloorToInt((end.z - begin.z) / step) + 1;       
+        buckets = new List<int>[sizeX * sizeY];
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            buckets[i] = new List<int>();
+        }
+    }
 
     public void AddAgent(Enemy enemy)
     {
@@ -43,6 +61,9 @@ public class HordeController : MonoBehaviour
 
     private void Update()
     {
+        ClearBuckets();
+        PrepareHashGrid();
+
         for (int i = 0; i < zombieMovements.Count; i++)
         {
             Vector3 flockVel = flocking.active ? Flock(zombieMovements[i], flocking.distance, flocking.power) : Vector3.zero;
@@ -57,18 +78,57 @@ public class HordeController : MonoBehaviour
         }
     }
 
+    void PrepareHashGrid()
+    {
+        ClearBuckets();
+        for (int i = 0; i < zombieMovements.Count; i++)
+        {
+            int x = Mathf.FloorToInt((zombieMovements[i].transform.position.x - begin.x) / step);
+            int y = Mathf.FloorToInt((zombieMovements[i].transform.position.z - begin.z) / step);
+            int idx = GetIdx(x, y);
+            buckets[idx].Add(i);
+        }
+    }
+
+    void ClearBuckets()
+    {
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            buckets[i].Clear();
+        }
+    }
+
+    int GetIdx(int x, int y)
+    {
+        return y * sizeX + x;
+    }
+
     Vector3 Flock(ZombieMovement zombieMovement, float distance, float power)
     {
         Vector3 center = Vector3.zero;
         int count = 0;
-        for (int i = 0; i < zombieMovements.Count; i++)
+
+        int x1 = Mathf.FloorToInt((zombieMovement.transform.position.x - distance - begin.x) / step);
+        int y1 = Mathf.FloorToInt((zombieMovement.transform.position.z - distance - begin.z) / step);
+        int x2 = Mathf.FloorToInt((zombieMovement.transform.position.x + distance - begin.x) / step);
+        int y2 = Mathf.FloorToInt((zombieMovement.transform.position.z + distance - begin.z) / step);
+
+        for (int x = x1; x <= x2; x++)
         {
-            if (Vector3.Distance(zombieMovement.transform.position, zombieMovements[i].transform.position) < distance)
+            for (int y = y1; y <= y2; y++)
             {
-                center += zombieMovements[i].transform.position;
-                count++;
+                int idx = GetIdx(x, y);
+                List<int> elts = buckets[idx];
+                for (int i = 0; i < elts.Count; i++)
+                {
+                    if (Vector3.Distance(zombieMovement.transform.position, zombieMovements[elts[i]].transform.position) < distance)
+                    {
+                        center += zombieMovements[elts[i]].transform.position;
+                        count++;
+                    }
+                }
             }
-        }
+        }        
         center /= (float)count;
         Vector3 dir = center - zombieMovement.transform.position;
         return dir.SetY(0f) * power;
@@ -78,12 +138,26 @@ public class HordeController : MonoBehaviour
     {
         Vector3 sumVel = Vector3.zero;
         int count = 0;
-        for (int i = 0; i < zombieMovements.Count; i++)
+
+        int x1 = Mathf.FloorToInt((zombieMovement.transform.position.x - distance - begin.x) / step);
+        int y1 = Mathf.FloorToInt((zombieMovement.transform.position.z - distance - begin.z) / step);
+        int x2 = Mathf.FloorToInt((zombieMovement.transform.position.x + distance - begin.x) / step);
+        int y2 = Mathf.FloorToInt((zombieMovement.transform.position.z + distance - begin.z) / step);
+
+        for (int x = x1; x <= x2; x++)
         {
-            if (Vector3.Distance(zombieMovement.transform.position, zombieMovements[i].transform.position) < distance)
+            for (int y = y1; y <= y2; y++)
             {
-                sumVel += zombieMovements[i].Agent.desiredVelocity;
-                count++;
+                int idx = GetIdx(x, y);
+                List<int> elts = buckets[idx];
+                for (int i = 0; i < elts.Count; i++)
+                {
+                    if (Vector3.Distance(zombieMovement.transform.position, zombieMovements[elts[i]].transform.position) < distance)
+                    {
+                        sumVel += zombieMovements[elts[i]].Agent.desiredVelocity;
+                        count++;
+                    }
+                }
             }
         }
         sumVel /= (float)count;
@@ -98,14 +172,27 @@ public class HordeController : MonoBehaviour
     {
         Vector3 sumCloseness = Vector3.zero;
 
-        for (int i = 0; i < zombieMovements.Count; i++)
+        int x1 = Mathf.FloorToInt((zombieMovement.transform.position.x - distance - begin.x) / step);
+        int y1 = Mathf.FloorToInt((zombieMovement.transform.position.z - distance - begin.z) / step);
+        int x2 = Mathf.FloorToInt((zombieMovement.transform.position.x + distance - begin.x) / step);
+        int y2 = Mathf.FloorToInt((zombieMovement.transform.position.z + distance - begin.z) / step);
+
+        for (int x = x1; x <= x2; x++)
         {
-            float dist = Vector3.Distance(zombieMovement.transform.position, zombieMovements[i].transform.position);
-            if (dist < distance)
+            for (int y = y1; y <= y2; y++)
             {
-                float closeness = distance - dist;
-                Vector3 dir = zombieMovement.transform.position - zombieMovements[i].transform.position;
-                sumCloseness += dir * closeness;
+                int idx = GetIdx(x, y);
+                List<int> elts = buckets[idx];
+                for (int i = 0; i < elts.Count; i++)
+                {
+                    float dist = Vector3.Distance(zombieMovement.transform.position, zombieMovements[elts[i]].transform.position);
+                    if (dist < distance)
+                    {
+                        float closeness = distance - dist;
+                        Vector3 dir = zombieMovement.transform.position - zombieMovements[elts[i]].transform.position;
+                        sumCloseness += dir * closeness;
+                    }
+                }
             }
         }
         return sumCloseness.SetY(0f) * power;
