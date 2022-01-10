0965b5bb25adf8edc147eb2637bbfab671b6a316
Alexander Abramov <rekunigato@gmail.com>
2022-01-10 19:41:14 +0300

    -Kept ya waitin ha

--- Assets/Scripts/Base/UI/HQLevelUI.cs
+++ Assets/Scripts/Base/UI/HQLevelUI.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -14,18 +16,26 @@ public class HQLevelUI : MonoBehaviour
     [SerializeField] TMP_Text pointCountText;
     [SerializeField] Transform pointCountTransform;
     [SerializeField] float disappearTime = 4f;
-
     HQBuilding hq;
     float currentDisappearTime = 0f;
     int pointCombo;
     Tween pointCountTween;
 
+    [SerializeField] private Transform giftsSpawnPoint;
+    [SerializeField] GameObject rewardPrefab;
+    [SerializeField] private Sprite openRewardSprite;
+    int rewardSegment;
+
     private void Start()
     {
         hq = FindObjectOfType<HQBuilding>();
         hq.OnPointAdded += Hq_OnPointAdded;
+        hq.OnLevelUp += Hq_OnLevelUp;
+        hq.OnRewardOpened += Hq_OnRewardOpened;
         pointCountTransform.localScale = Vector3.zero;
         UpdateProgressBar();
+        RewardPlacement();
+        //UpdateRewards();
     }
 
     private void Hq_OnPointAdded(int value)
@@ -47,7 +57,7 @@ public class HQLevelUI : MonoBehaviour
     }
 
     void UpdateProgressBar()
-    {       
+    {
         progressionImage.fillAmount = ((float)hq.CurrentCount / (float)hq.Cost);
         currentLevelText.text = hq.Level.ToString();
         nextLevelText.text = (hq.Level + 1).ToString();
@@ -67,5 +77,64 @@ public class HQLevelUI : MonoBehaviour
         pointCountTween = pointCountTransform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.OutCirc);
     }
 
+    // private void RewardPlacement()
+    // {
+    //     Transform levelBar = transform.GetChild(0);
+    //     int width = (int) levelBar.GetComponent<RectTransform>().sizeDelta.x;
+    //     int segment = width / (hq.RewardCount + 1);
+    //     Vector3 startPos = levelBar.position - new Vector3(width / 2, 0, 0);
+    //     
+    //     for (int i = 0; i < hq.RewardCount; i++)
+    //         Instantiate(rewardPrefab, startPos + new Vector3(segment * (i + 1), 20, 0), Quaternion.identity, levelBar);
+    // }
 
+    private void Hq_OnLevelUp()
+    {
+        RewardRemove();
+        RewardPlacement();
+    }
+    private void UpdateRewards()
+    {
+        for (int i = 1; i <= hq.RewardCount; i++)
+        {
+            if (hq.CurrentCount >= rewardSegment * i)
+            {
+                while (i > 0)
+                    giftsSpawnPoint.GetChild(--i).GetChild(0).GetComponent<Image>().sprite = openRewardSprite;
+                break;
+            }
+        }
+    }
+
+    private void Hq_OnRewardOpened(int index)
+    {
+        giftsSpawnPoint.GetChild(index).GetChild(0).GetComponent<Image>().sprite = openRewardSprite;
+    }
+    
+    private void RewardPlacement()
+    {
+        Transform levelBar = transform.GetChild(0); 
+        float width = levelBar.GetComponent<RectTransform>().sizeDelta.x;
+        float segmentWidth = width / hq.Cost;
+        rewardSegment = hq.Cost / (hq.RewardCount + 1);
+        Vector3 startPos = levelBar.position - new Vector3(width / 2, 0, 0);
+        int nextChest = hq.NextChest;
+
+        for (int i = 0; i < hq.RewardCount; i++)
+        {
+            Instantiate(rewardPrefab, startPos + new Vector3(segmentWidth * rewardSegment * (i + 1), 20, 0),
+                Quaternion.identity, giftsSpawnPoint);
+            if (i < nextChest)
+            {
+                giftsSpawnPoint.GetChild(i).GetChild(0).GetComponent<Image>().sprite = openRewardSprite;
+            }
+        }
+    }
+
+    private void RewardRemove()
+    {
+        foreach (Transform child in giftsSpawnPoint.transform) {
+            Destroy(child.gameObject);
+        }
+    }
 }
