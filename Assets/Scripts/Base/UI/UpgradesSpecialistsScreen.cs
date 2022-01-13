2d78df80fd3580b42933dab8f032dca11784230e
Alexander Abramov <rekunigato@gmail.com>
2022-01-13 19:18:40 +0300

    - upd

--- Assets/Scripts/Base/UI/UpgradesSpecialistsScreen.cs
+++ Assets/Scripts/Base/UI/UpgradesSpecialistsScreen.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.PlayerLoop;
 
 public class UpgradesSpecialistsScreen : UIScreen, IShowScreen
 {
     [SerializeField] CanvasGroup canvasGroup;
     [SerializeField] TMP_Text label;
-    [SerializeField] List<StatUpgradeCard> cards;
+    private CardController cardController;
+    private List<GameObject> cards = new List<GameObject>();
     [SerializeField] Transform panel;
 
     List<(StatsType, StatInfo)> stats = new List<(StatsType, StatInfo)>();
     ResourcesInfo availableResources;
     System.Action onClose;
-    
+
+    private void Awake()
+    {
+        cardController = FindObjectOfType<CardController>();
+    }
+
     public void Show(UpgradeZone zone, string name, List<(StatsType, StatInfo)> stats, ResourcesInfo availableResources, Action onClose = null)
     {
         this.onClose = onClose;
         label.text = name;
         this.stats = stats;
         this.availableResources = availableResources;
+        UpdateCards();
+    }
+
+    void UpdateCards()
+    {
+        Debug.Log("Cards count: "+cardController.ActiveCards.Count);
     }
 }
