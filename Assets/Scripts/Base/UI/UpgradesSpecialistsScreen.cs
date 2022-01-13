a17cd7276197897aa15e1515ae6c4e31362a919e
Alexander Abramov <rekunigato@gmail.com>
2022-01-13 20:47:39 +0300

    - upd

--- Assets/Scripts/Base/UI/UpgradesSpecialistsScreen.cs
+++ Assets/Scripts/Base/UI/UpgradesSpecialistsScreen.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 
 public class UpgradesSpecialistsScreen : UIScreen, IShowScreen
 {
     [SerializeField] CanvasGroup canvasGroup;
     [SerializeField] TMP_Text label;
     private CardController cardController;
+    [SerializeField] private GameObject cardPrefab;
     private List<GameObject> cards = new List<GameObject>();
     [SerializeField] Transform panel;
+    [SerializeField] private Transform cardsSpawnPoint;
 
     List<(StatsType, StatInfo)> stats = new List<(StatsType, StatInfo)>();
     ResourcesInfo availableResources;
     System.Action onClose;
-
+    private CardsInfo activeCards => cardController.ActiveCards;
+    
     private void Awake()
     {
         cardController = FindObjectOfType<CardController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            UpdateCards();
     }
 
     public void Show(UpgradeZone zone, string name, List<(StatsType, StatInfo)> stats, ResourcesInfo availableResources, Action onClose = null)
@@ -32,6 +42,24 @@ public class UpgradesSpecialistsScreen : UIScreen, IShowScreen
 
     void UpdateCards()
     {
-        Debug.Log("Cards count: "+cardController.ActiveCards.Count);
+        int i = 0;
+        for (; i < activeCards.Count; i++)
+        {
+            if (i < cards.Count)
+                cards[i].SetActive(true);
+            else
+                cards.Add(Instantiate(cardPrefab, cardsSpawnPoint));
+            CardSlot cardSlot = activeCards.cardSlots[i];
+            // CardStatsSlot stats = cardController.CardStats(activeCards.cardSlots[i].card);
+            // var values =  stats.statsInfo.Values;
+            // var keys =  stats.statsInfo.Keys;
+            // Debug.Log();
+            // cards[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = stats.statsInfo[cardSlot.card.CardStatsSettings[i]]; 
+            cards[i].transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "lvl " + cardSlot.level.ToString(); 
+            cards[i].transform.GetChild(2).GetComponent<Image>().sprite = cardSlot.card.Icon;
+        }
+
+        for (; i < cards.Count; i++)
+            cards[i].SetActive(false);
     }
 }
