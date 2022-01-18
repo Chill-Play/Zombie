ef2c0811d1c49354d2a57796bba36a79f22341f1
Alexander Abramov <rekunigato@gmail.com>
2022-01-18 17:07:20 +0300

    - upd

---
+++ Assets/Scripts/Base/UI/SellResourceButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SellResourceButton : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI priceText;
+    [SerializeField] private TextMeshProUGUI countText;
+    [SerializeField] private Image money;
+    [SerializeField] private Image resource;
+    private System.Action onBuy;
+
+    private void Show()
+    {
+        transform.localScale = Vector3.zero;
+        transform.DOScale(Vector3.one,.1f).SetEase(Ease.InElastic, 1.1f, .3f);
+    }
+
+    private void Hide()
+    {
+        transform.DOScale(Vector3.zero,.1f).SetEase(Ease.InElastic, 1.1f, .3f).OnComplete(() => { gameObject.SetActive(false); });
+    }
+
+    public void Setup(ResourceType resourceType, int count,int price, System.Action click)
+    {
+        onBuy += click;
+        priceText.text = price.ToString();
+        money.sprite = ResourcesController.Instance.OpenedResources[0].icon;
+        resource.sprite = resourceType.icon;
+        countText.text = count.ToString();
+        Show();
+    }
+
+    public void Buy()
+    {
+        onBuy?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Base/UI/UIShopScreenView.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class UIShopScreenView : MonoBehaviour
+{
+    [SerializeField] private Transform shop;
+    [SerializeField] private Transform buttons;
+    [SerializeField] private Vector3 alignment;
+    [SerializeField] private GetResourceButton[] getResourceButtons;
+    [SerializeField] private SellResourceButton[] sellResourceButtons;
+    [SerializeField] private int sellPercent;
+    [SerializeField] private int ADSPercent;
+    [SerializeField] private int minSellCount;
+    private List<ResourceType> resources = new List<ResourceType>();
+    private void Awake()
+    {
+        resources = FindObjectOfType<ResourcesController>().OpenedResources;
+        UpdateSellButtons();
+        UpdateGetResourceButtons();
+    }
+
+    void UpdateSellButtons()
+    {
+        foreach (var button in sellResourceButtons)
+        {
+            ResourceType resourceType = resources[Random.Range(1, resources.Count)];
+            var playerResources = ResourcesController.Instance.ResourcesCount;
+            int count = Mathf.Max(minSellCount, playerResources.Count(resourceType) * sellPercent/ 100);
+            //int count =playerResources.Count(resourceType) * sellPercent / 100;
+            int price = count * resourceType.price;
+            button.Setup(resourceType, count, price, () =>
+            {
+                if (playerResources.Count(resourceType) >= count)
+                {
+                    playerResources.Subtract(resourceType, count);
+                    playerResources.Add(resources[0], price);
+                    ResourcesController.Instance.UpdateResources();
+                }
+            });
+        }
+    }
+
+    void UpdateGetResourceButtons()
+    {
+        foreach (var button in getResourceButtons)
+        {
+            ResourceType resourceType = resources[Random.Range(0, resources.Count)];
+            var playerResources = ResourcesController.Instance.ResourcesCount;
+            int count = Mathf.Max(minSellCount, playerResources.Count(resourceType) * ADSPercent/ 100);
+            button.Setup(resourceType, count, () =>
+            {
+                playerResources.Add(resourceType, count);
+                ResourcesController.Instance.UpdateResources();
+            });
+        }
+    }
+    
+    void LateUpdate()
+    {
+        if (shop)
+        {
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(shop.transform.position);
+            screenPos.z = 0f;
+            buttons.position = screenPos + alignment;
+        }
+    }
+}
