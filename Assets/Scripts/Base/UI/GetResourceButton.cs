26b9d3829e80fdad4eefd64c8e998556545c0291
Alexander Abramov <rekunigato@gmail.com>
2022-01-19 15:08:02 +0300

    -upd

--- Assets/Scripts/Base/UI/GetResourceButton.cs
+++ Assets/Scripts/Base/UI/GetResourceButton.cs
@@ -5,29 +5,18 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 using DG.Tweening;
 
-public class GetResourceButton : MonoBehaviour
+public class GetResourceButton : ShopButton
 {
     [SerializeField] private TextMeshProUGUI countText;
     [SerializeField] private Image resource;
     private System.Action onAdsShowed;
-
-    private void Show()
-    {
-        transform.localScale = Vector3.zero;
-        transform.DOScale(Vector3.one,.1f).SetEase(Ease.InElastic, 1.1f, .3f);
-    }
-
-    private void Hide()
-    {
-        transform.DOScale(Vector3.zero,.1f).SetEase(Ease.InElastic, 1.1f, .3f).OnComplete(() => { gameObject.SetActive(false); });
-    }
+    
 
     public void Setup(ResourceType resourceType, int count, System.Action click)
     {
         onAdsShowed += click;
         resource.sprite = resourceType.icon;
         countText.text = count.ToString();
-        Show();
     }
 
     public void ShowADS()
---
+++ Assets/Scripts/Base/UI/ShopButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class ShopButton : MonoBehaviour
+{ 
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        transform.localScale = Vector3.zero;
+        transform.DOScale(Vector3.one,.1f).SetEase(Ease.InElastic, 1.1f, .3f);
+    }
+
+    public void Hide()
+    {
+        transform.DOScale(Vector3.zero,.1f).SetEase(Ease.InElastic, 1.1f, .3f).OnComplete(() => { gameObject.SetActive(false); });
+    }
+}
