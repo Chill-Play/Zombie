58d244de0bf8919d583159c8f68f6255fc89658a
Alexander Abramov <rekunigato@gmail.com>
2022-01-26 10:35:56 +0300

    - upd

--- Assets/Scripts/Raids/UI/UIStarsAnimation.cs
+++ Assets/Scripts/Raids/UI/UIStarsAnimation.cs
@@ -1,9 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-namespace Raids.UI
+public class UIStarsAnimation : MonoBehaviour
 {
-    public class UIStarsAnimation : MonoBehaviour
+    [SerializeField] private Animator[] stars;
+    [SerializeField] private float duration;
+    [SerializeField] private int count;
+    private float timer;
+    private int currentStar;
+
+    private void Start()
     {
-        
+        timer = duration;
+    }
+
+    private void Update()
+    {
+        LaunchStars();
+    }
+
+    public void LaunchStars()
+    {
+        if (timer <= 0)
+        {
+            for (int i = 0; i < count; i++)
+                stars[(i + currentStar) % stars.Length].gameObject.SetActive(true);
+            timer = duration;
+            currentStar += count;
+        }
+        else
+            timer -= Time.deltaTime;
     }
 }
