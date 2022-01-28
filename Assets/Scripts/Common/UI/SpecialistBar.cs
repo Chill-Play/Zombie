d9523aa02d702fe1df3043b94d84a68592d6da4d
Alexander Abramov <rekunigato@gmail.com>
2022-01-28 12:49:28 +0300

    - upd

---
+++ Assets/Scripts/Common/UI/SpecialistBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpecialistBar : MonoBehaviour
+{
+    [SerializeField] Image icon;
+    [SerializeField] TMP_Text text;
+
+    public void Setup(Card card)
+    {
+        icon.sprite = card.Icon;
+        text.text = card.CardName;
+    }
+}
