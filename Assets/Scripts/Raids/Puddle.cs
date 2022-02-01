9f92e72faf417735ddad6c3e28643b8c347b71bb
Alexander Abramov <rekunigato@gmail.com>
2022-02-01 14:29:33 +0300

    - add new logic for Big zombie and zombie Puddle

---
+++ Assets/Scripts/Raids/Puddle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using DG.Tweening;
+using UnityEditor;
+using UnityEngine;
+
+public class Puddle : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    [SerializeField] private float speed;
+    [SerializeField] private float lifeTime = 3;
+    
+    private float tmpSpeed;
+    
+    private void Start()
+    {
+        Vector3 scale = transform.localScale;
+        
+        transform.localScale = Vector3.zero;
+        tmpSpeed = speed;
+        var seq = DOTween.Sequence();
+        seq.Append(transform.DOScale(scale, .75f));
+        seq.AppendInterval(lifeTime);
+        seq.Append(transform.DOScale(Vector3.zero, .5f));
+        seq.OnComplete(()=>Destroy(gameObject));
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.TryGetComponent(out IDamagable damagable))
+        {
+            tmpSpeed -= Time.deltaTime;
+            if (tmpSpeed > 0 || other.TryGetComponent(out Enemy enemy))
+                return;
+            Hit(damagable);
+            tmpSpeed = speed;
+        }
+    }
+
+    public void Hit(IDamagable damagable)
+    {
+        DamageInfo damageInfo = new DamageInfo()
+        {
+            direction = transform.forward,
+            damage = damage,
+        };
+        damagable.Damage(damageInfo);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.color *= new Vector4(1, 1, 1, .5f);
+        Gizmos.DrawSphere(transform.position , GetComponent<SphereCollider>().radius);
+    }
+}
---
+++ Assets/Scripts/Raids/Zombies/ZombieSpawnGameobjectOnDeath.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ZombieSpawnGameobjectOnDeath : MonoBehaviour
+{
+    [SerializeField] UnitHealth health;
+    [SerializeField] GameObject gameObject;
+    
+    void OnEnable()
+    {
+        health.OnDead += Health_OnDead;
+    }
+
+    private void OnDisable()
+    {
+        health.OnDead -= Health_OnDead;
+    }
+    
+    private void Health_OnDead(EventMessage<Empty> obj)
+    {
+        SpawnGameobject();
+    }
+
+    void SpawnGameobject()
+    {
+        Instantiate(gameObject, transform.position, Quaternion.identity);
+    }
+}
