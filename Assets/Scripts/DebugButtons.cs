a51159f3137a2a0fb10d95ab0af40204e89b66ce
Alexander Abramov <rekunigato@gmail.com>
2022-02-08 16:45:47 +0300

    - upd

---
+++ Assets/Scripts/DebugButtons.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+public class DebugButtons : MonoBehaviour
+{
+    [SerializeField] private bool showButtons;
+    [SerializeField] private GameObject[] debugUI;
+
+    private HQBuilding hq;
+
+    private void Start()
+    {
+        if (showButtons)
+        {
+            hq = FindObjectOfType<HQBuilding>();
+            foreach (var ui in debugUI)
+            {
+                ui.SetActive(true);
+            }
+        }
+    }
+
+    public void AddNoiseLevel()
+    {
+        if (NoiseController.Instance)
+            NoiseController.Instance.AddNoiseLevel(99999f);
+    }
+
+    public void KillZombies()
+    {
+        if (ZombieWaveSpawner.Instance)
+        {
+            ZombieWaveSpawner.Instance.StopSpawning();
+            ZombieWaveSpawner.Instance.ExecudeAll();
+        }
+    }
+
+    public void AddPointLevel()
+    {
+        hq.AddPoint(1);
+    }
+
+    public void LevelUp()
+    {
+        hq.LevelUp();
+    }
+
+    public void AddResources()
+    {
+        foreach(var slot in ResourcesController.Instance.ResourcesCount.Slots)
+        {
+            slot.count += 100;
+        }
+        ResourcesController.Instance.UpdateResources();
+    }
+
+    public void Suicide()
+    {
+        Squad.Instance.Units[0].GetComponent<UnitHealth>().TakeDamage(1000f, Vector3.forward);
+    }
+}
